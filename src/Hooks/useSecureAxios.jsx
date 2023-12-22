d58a52c283808552f8f5a099fdd0fae2902edfb0
Masum-Reza <careerhunter4280@gmail.com>
2023-12-22 18:08:25 +0600

    replaced with production server

--- src/Hooks/useSecureAxios.jsx
+++ src/Hooks/useSecureAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://todo-server-inky.vercel.app',
     headers: { token: localStorage.getItem('token') }
 });
 
